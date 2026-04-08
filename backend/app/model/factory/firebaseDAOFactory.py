20f77f1db1083f8f19ac384e00e93886a7ca7e6c
Pxbloo <104941705+Pxbloo@users.noreply.github.com>
2026-04-08 12:25:38 +0200

    feta(factory): correct FirebaseDAOFactory (FirebaseMerchandisingDAO is not implemented yet, but will work then)

--- backend/app/model/factory/firebaseDAOFactory.py
+++ backend/app/model/factory/firebaseDAOFactory.py
@@ -1,26 +1,32 @@
 from .interfaceDAOFactory import InterfaceDAOFactory
 from ..dao.firebase.firebase_festival_dao import FirebaseFestivalDAO
+from ..dao.firebase.firebase_user_dao import FirebaseUserDAO
+from ..dao.firebase.firebase_artist_dao import FirebaseArtistDAO
+from ..dao.firebase.firebase_blog_dao import FirebaseBlogDAO
+from ..dao.firebase.firebase_grounds_dao import FirebaseGroundDAO
+from ..dao.firebase.firebase_history_dao import FirebaseHistoryDAO
+from ..dao.firebase.firebase_merchandising_dao import FirebaseMerchansidingDAO
 
 
 class FirebaseDAOFactory(InterfaceDAOFactory):
 
-    def __init__(self) -> None:
-        # La configuración de credenciales la gestiona FirebaseConnector
-        # (FIREBASE_CREDENTIALS o serviceAccountKey.json por defecto)
-        pass
-
     def get_festival_dao(self) -> FirebaseFestivalDAO:
         return FirebaseFestivalDAO()
 
-    def get_user_dao(self):
-        raise NotImplementedError
-    def get_artist_dao(self):
-        raise NotImplementedError
-    def get_blog_post_dao(self):
-        raise NotImplementedError
-    def get_ground_dao(self):
-        raise NotImplementedError
-    def get_history_dao(self):
-        raise NotImplementedError
+    def get_user_dao(self) -> FirebaseUserDAO:
+        return FirebaseUserDAO()
+
+    def get_artist_dao(self) -> FirebaseArtistDAO:
+        return FirebaseArtistDAO()
+
+    def get_blog_post_dao(self) -> FirebaseBlogDAO:
+        return FirebaseBlogDAO()
+
+    def get_ground_dao(self) -> FirebaseGroundDAO:
+        return FirebaseGroundDAO()
+
+    def get_history_dao(self) -> FirebaseHistoryDAO:
+        return FirebaseHistoryDAO()
+
     def get_merchandising_dao(self):
-        raise NotImplementedError
+        return FirebaseMerchansidingDAO()
